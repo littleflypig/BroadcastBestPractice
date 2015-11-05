d49c4704755e787b66153c429c098196f168756d
littleflypig <tiancaidefaxian@qq.com>
2015-11-05 09:43:29 +0800

    HammingWeight

---
+++ HammingWeight.java
@@ -0,0 +1,18 @@
+package com.icbc.suanfa;
+
+public class Solution {
+	public int hammingWeight(int n){
+		String s=Integer.toBinaryString(n);
+		int count=0;
+		for(char c:s.toCharArray()){
+			if(c=='1'){
+				count++;
+			}
+		}
+		return count;
+	}
+}
+
+
+
+ 
